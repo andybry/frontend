36a5057ed48a3c3cec8ce45e1dd11aaf77e74d3f
vagrant <vagrant@vagrant-tm-lucid-64.config>
2013-03-28 17:20:03 +0000

    MIR-4697 Js file - Widget behaviours

---
+++ april-fools/april-folls.js
@@ -0,0 +1,26 @@
+ var _aprilfoolcontent = '<div class="april-fool-info">' +
+ '<div class="info"><img src="menu-text.jpg" ></div> </div>' +
+ '<div class="april-fool-menu"><div class="list"><ul>' +
+ '<li data-id="ancient"><a href="#">Ancient</a></li>' +
+ '<li data-id="ms-dos"><a href="#">MS Dos</a></li>' +
+ '<li data-id="drunk"><a href="#">Drunk</a></li>' +
+ '<li data-id="x-ray"><a href="#">X-Ray</a></li>' +
+ '<li data-id="backwards"><a href="#">Backwards</a></li>' +
+ '<li data-id="no-filter" class="last"><a href="#" class="btn-img">Clear Filter</a></li>' +
+ '</ul></div></div>';
+ 
+ $("body").prepend(_aprilfoolcontent);
+ 
+ $("head").append('<link rel="stylesheet" type="text/css" href="filters.css"/>');
+  
+ $(".april-fool-menu li").click(function(){
+  $(this).addClass("active");
+  $(this).siblings().removeClass("active");
+  $("body").removeClass();
+  var data = $(this).attr('data-id'); 
+  
+  $("body").addClass(data);
+ 
+ return false;
+ 
+ });
